43dee102ada953b51f7b4141d7bd844ccdf6a6be
Chandrasen <chandrasen265@gmail.com>
2025-06-18 17:51:53 -0400

    Set up server .env, update .gitignore, clean project structure

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product");
 const app = express();
+
+require("dotenv").config();
+
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("Connection error:", err));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
